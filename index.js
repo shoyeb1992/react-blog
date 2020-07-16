b5bf4b6ce3eed265536de4696d4c21fdb13e6ab3
Mohd Shoyeb <md8shoyeb@gmail.com>
2020-07-17 00:20:52 +0530

    Move private information into dev.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://shoyeb:abc1234@react-blog.lcmtn.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>console.log("Database is connected"))
+
+const {User} = require('./models/user');
+
+mongoose.connect(config.mongoURI, {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>console.log("Database is connected"))
 .catch(err=>console.log(err));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// API routes start
+app.post('/api/users/register', (req, res)=>{
+    const user = new User(req.body);
+    user.save((err,userData)=>{
+        if(err) return res.json({success:false, err});
+        return res.status(200).json({success:true});
+    })
+    
+})
+
 
 app.get('/home', (req, res)=>{
     res.send('Hello World')
